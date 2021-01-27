2d757139905bbb300944145d95b1deb8b166aaef
Michel Dos Santos Constantino <m.dsc@outlook.fr>
2021-01-27 16:19:23 +0100

    Ajout de la doc

--- 02-uWamp/www/my-app/controllers/sendMailRequest.php
+++ 02-uWamp/www/my-app/controllers/sendMailRequest.php
@@ -1,4 +1,18 @@
 <?php
 
-mail('michel.dossantos@eduvaud.ch','Test','Ceci est un test fdp');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Exception class. */
+require '../../../phpapps/phpmailer/src/Exception.php';
+
+/* The main PHPMailer class. */
+require '../../../phpapps/phpmailer/src/PHPMailer.php';
+
+/* SMTP class, needed if you want to use SMTP. */
+require '../../../phpapps/phpmailer/src/SMTP.php';
+
+$email = new PHPMailer(TRUE);
+
 ?>
--- 02-uWamp/www/my-app/models/dao.php
+++ 02-uWamp/www/my-app/models/dao.php
@@ -11,7 +11,7 @@
 
 class Database
 {
-
+    
     private $connector = null;
 
     public function Connect()
