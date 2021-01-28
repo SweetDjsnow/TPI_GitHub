dc22874ed10ff72c694eea783604eb369e79a24d
Michel Dos Santos Constantino <m.dsc@outlook.fr>
2021-01-28 12:07:47 +0100

    format mail + ajout extension mailparse

--- 02-uWamp/www/my-app/controllers/sendMailRequest.php
+++ 02-uWamp/www/my-app/controllers/sendMailRequest.php
@@ -1,20 +1,37 @@
 <?php
 
 $to = "michel.dossantos@eduvaud.ch";
-$subject = "TEST";
-$msg = "First line of text\nSecond line of text";
+
+$subject = "Demande d'ajout pour ".$_POST['cityName'];
+
+$msg = base64_encode("Demande de création de compte pour ".$_POST['cityName']."\r\n
+Prénom : ".$_POST['firstName']."\r\n
+Nom : ".$_POST['lastName']."\r\n
+Email : ".$_POST['email']."\r\n
+Numero de téléphone : ".$_POST['phoneNumber']."\r\n
+Addresse du bureau : ".$_POST['officeAddress']."\r\n
+Code postal : ".$_POST['npa']."\r\n");
+
+//$msg .= "Prénom : ".$_POST['firstName']."\r\n";
+//$msg .= "Nom : ".$_POST['lastName']."\r\n";
+//$msg .= "Email : ".$_POST['email']."\r\n";
+//$msg .= "Numero de téléphone : ".$_POST['phoneNumber']."\r\n";
+//$msg .= "Addresse du bureau : ".$_POST['officeAddress']."\r\n";
+//$msg .= "Code postal : ".$_POST['npa']."\r\n";
+
 
 $name = "Michel Dos Santos";
 $from = "michel.dossantos@eduvaud.ch";
 
 $headers  = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$headers .= "Content-type: text/plain; charset=utf-8\r\n";
+$headers .= "Content-Transfer-Encoding: base64\r\n";
 $headers .= "From: {$name} <{$from}>\r\n";
 $headers .= "Reply-To: <{$from}>\r\n";
 $headers .= "X-Mailer: PHP/".phpversion()."\r\n";
 
 // use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+//$msg = wordwrap($msg,70);
 
 // send email
 try
--- 02-uWamp/www/my-app/views/inscription.php
+++ 02-uWamp/www/my-app/views/inscription.php
@@ -11,17 +11,45 @@
 
             <div class="form">
                 <form class="login-form" action="../controllers/sendMailRequest.php" method="POST">
-                    <input type="text" name="cityName" placeholder="Nom de la commune"/>
-                    <input type="text" name="firstName" placeholder="Prénom"/>
-                    <input type="text" name="lastName" placeholder="Nom"/>
-                    <input type="text" name="email" placeholder="Adresse email de contact"/>
-                    <input type="text" name="phoneNumber" placeholder="Numéro de téléphone"/>
-                    <input type="text" name="officeAddress" placeholder="Addresse du bureau"/>
-                    <input type="text" name="npa" placeholder="Code postal"/>
+                    <input type="text" name="cityName" placeholder="Nom de la commune" required/>
+                    <input type="text" name="firstName" placeholder="Prénom" required/>
+                    <input type="text" name="lastName" placeholder="Nom" required/>
+                    <input type="text" name="email" placeholder="Adresse email de contact" required/>
+                    <input type="text" name="phoneNumber" placeholder="Numéro de téléphone" required/>
+                    <input type="text" name="officeAddress" placeholder="Addresse du bureau" required/>
+                    <input type="text" name="npa" placeholder="Code postal" required/>
                     <button>Envoyer demande</button>
                 </form>
         </div>
 
+        <?php
+            $mbox = imap_open("{outlook.office365.com:993/imap/ssl}", "michel.dossantos@eduvaud.ch", "1qay2wsxRex998...!?");
+
+            echo "<h1>Mailboxes</h1>\n";
+            $folders = imap_listmailbox($mbox, "{outlook.office365.com:993/imap/ssl}", "*");
+            
+            if ($folders == false) {
+                echo "Appel échoué<br />\n";
+            } else {
+                foreach ($folders as $val) {
+                    echo $val . "<br />\n";
+                }
+            }
+            
+            echo "<h1>en-têtes dans INBOX</h1>\n";
+            $headers = imap_headers($mbox);
+            
+            if ($headers == false) {
+                echo "Appel échoué<br />\n";
+            } else {
+                foreach ($headers as $val) {
+                    echo $val . "<br />\n";
+                }
+            }
+            
+            imap_close($mbox);
+            echo phpinfo();
+        ?>
 
     </body>
 
