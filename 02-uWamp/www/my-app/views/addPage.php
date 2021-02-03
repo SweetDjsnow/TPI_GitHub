1f5e735eba3f0c547d77d7e0e1d7097bedfc3d19
Michel Dos Santos Constantino <m.dsc@outlook.fr>
2021-02-03 12:13:22 +0100

    doc

--- 02-uWamp/www/my-app/views/addPage.php
+++ 02-uWamp/www/my-app/views/addPage.php
@@ -9,15 +9,22 @@
 
         <div class="signup-page">
 
-            <div class="form"></div>
+            <div class="form">
+                <form class="login-form" action="#" method="POST" enctype="multipart/form-data">
+                    <label for="image">Image:</label>
+                    <input type="file" name="image" id="image" /><br />
+                    <input type="submit" name="submit" value="Submit" />
+                </form>
+            </div>
                 
-        </div>
-
-        <div class="back-btn-div">
+            <div class="back-btn-div">
         <a href="../views/mainPage.php">
             <button class="back-btn"><img src="../img/left-arrow.png" alt="Back Arrow"></button>
         </a>
         </div>
+        </div>
+
+        
 
     </body>
 
