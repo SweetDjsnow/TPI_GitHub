10662c31c40af6a7154e230f78d2cce22534e100
Michel Dos Santos Constantino <m.dsc@outlook.fr>
2021-02-18 16:29:08 +0100

    doc + page donneur / receveur

--- 02-uWamp/www/my-app/views/retrievePage.php
+++ 02-uWamp/www/my-app/views/retrievePage.php
@@ -17,51 +17,88 @@
         <h1 class="retrieve-page-title">Définir le receveur et donneur du vélo</h1>
         <div class="retrieve-page-form">
             <div class="form">
-                <div>
-                    
-                    <form action="../views/searchPage.php" method="POST">
-                        <label>Receveur:</label>
-                        <div class="select">
-                            <select name="retriever" id="slct">
-                                <?php
-                                    $retrievers = $dao->GetAllReceiver();
-                                    foreach($retrievers as $key => $value)
-                                    {
-                                        echo '<option value = "'.$value['idReceiver'].'">'.$value['recFirstName']." ".$value['recLastName'].'</option>';
-                                    }
-                                ?>
-                            </select>
-                        </div>
-
-                        <div class="retrieve-link">
-                            <a href="#" style="width: 100%;">Créer receveur</a>
-                        </div>
-
-
-                        <label>Donneur:</label>
+                <table style="width: 100%">
+                    <tr>
+                        <th>Receveur</th>
+                        <th>Donneur</th>
+                    </tr>
+                    <form action="#" method="POST">
+                        <tr>
+                            <td>
+                                <div class="select">
+                                    <select name="retriever" id="slct">
+                                        <?php
+                                            $retrievers = $dao->GetAllReceiver();
+                                            foreach($retrievers as $key => $value)
+                                            {
+                                                echo '<option value = "'.$value['idReceiver'].'">'.$value['recFirstName']." ".$value['recLastName'].'</option>';
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="select">
+                                    <select name="giver" id="slct">
+                                        <?php
+                                            $givers = $dao->GetAllGiver();
+                                            foreach($givers as $key => $value)
+                                            {
+                                                echo '<option value = "'.$value['idGiver'].'">'.$value['givFirstName']." ".$value['givLastName'].'</option>';
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
+                            </td>
+                        </tr>
                         
-                        <div class="select">
-                            <select name="giver" id="slct">
-                                <?php
-                                    $givers = $dao->GetAllGiver();
-                                    foreach($givers as $key => $value)
-                                    {
-                                        echo '<option value = "'.$value['idGiver'].'">'.$value['givFirstName']." ".$value['givLastName'].'</option>';
-                                    }
-                                ?>
-                            </select>
-                        </div>
-
-                        <div class="retrieve-link">
-                            <a href="#">Créer donneur</a>
-                        </div>
-
-                        <button>Rendre</button>
-
+                        <tr>
+                            <td colspan="2">
+                                <button style="width: 60%;margin-bottom: 20px; margin-top: 10px;">Rendre</button>
+                            </td>
+                        </tr>
                     </form>
-                </div>
+                    <tr>
+                        <td colspan="2" style="padding-bottom: 30px;"></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Ajouter un receveur
+                        </td>
+                        <td>
+                            Ajouter un donneur
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-right: solid gray 2px;">
+                            <form action="../views/searchPage.php" method="POST" class="create-form">
+                                <input type="text" placeholder="Prénom" name="firstName">
+                                <input type="text" placeholder="Nom de famille" name="lastName">
+                                <input type="text" placeholder="Adresse email" name="email">
+                                <input type="text" placeholder="Numéro de téléphone" name="phoneNumber">
+                                <button style="width: 60%;">Ajouter</button>
+                            </form>
+                        </td>
+                        <td>
+                            <form action="../views/searchPage.php" method="POST" class="create-form">
+                                <input type="text" placeholder="Prénom" name="firstName">
+                                <input type="text" placeholder="Nom de famille" name="lastName">
+                                <input type="text" placeholder="Adresse email" name="email">
+                                <input type="text" placeholder="Numéro de téléphone" name="phoneNumber">
+                                <button style="width: 60%;">Ajouter</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                    
+                    </tr>
+                </table>
+                
                 
             </div>
+            
+            
+
 
             <div class="back-btn-div">
                 <a href="../views/mainPage.php">
